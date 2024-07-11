ce5d0375a443a16854a4f3e47b2051936a5ac9a7
mkemaln <kemalmuhammad2509@gmail.com>
2024-07-11 19:13:18 +0700

    Setup cors adn body-parser on main file

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
 const port = 3000
 
 app.get('/', (req, res) => {
